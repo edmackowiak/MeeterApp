5cedc9ebf775c39bb5451a8282846c43c04ad9f4
Sawyer H <s.hollenshead@gmail.com>
2012-03-23 21:35:08 -0400

    Front-end - start

---
+++ front-end/js/scripts-ck.js
@@ -0,0 +1,1 @@
+$(function(){$(window).load(function(){var a=$(".page").height();a<parseInt($(".page").css("min-height"))&&(a=parseInt($(".page").css("min-height"))+"px");$("section.sidebar").css("min-height",a)})});
