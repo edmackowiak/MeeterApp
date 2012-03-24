1aa41bb545768c9ea02d988fd01fb5f7f85eab7e
Ed Mackowiak <edmackowiak@gmail.com>
2012-03-24 10:18:09 -0700

    removed resource tag from dashbaord tag

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.instance_of?(User)
-      dashboard_path(resource)
+      dashboard_path
     else
       super
     end
