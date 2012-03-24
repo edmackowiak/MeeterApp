682315d72cc17740b8e28140f840c0c0f6a789ff
Sawyer H <s.hollenshead@gmail.com>
2012-03-24 11:35:16 -0400

    Styling for sessions and home

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def body_classes
+        @body_classes ||= [controller.controller_name, controller.action_name]
+        @body_classes << 'admin' if request.path.starts_with? "/admin"
+        return @body_classes
+    end
 end
