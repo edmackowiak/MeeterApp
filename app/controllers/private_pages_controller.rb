626f046b0e9b1e774b39ecc4d1445542513197ce
Ed Mackowiak <edmackowiak@gmail.com>
2012-03-24 23:46:40 -0700

    set up dashboard

--- app/controllers/private_pages_controller.rb
+++ app/controllers/private_pages_controller.rb
@@ -5,7 +5,22 @@ class PrivatePagesController < ApplicationController
   before_filter :authenticate_user!
 
   def dashboard
-    
+    @number_of_meetings = Meeting.count
+
+    @total_cost = 0
+    @month_cost = Array.new(12)
+
+    for i in 1..12
+      @month_cost[i] = 0
+    end
+
+    Meeting.all.each do |m|
+      @total_cost += m.cost
+      month = m.date.month
+      @month_cost[month] += m.cost
+    end
+
+
   end
 
 end
