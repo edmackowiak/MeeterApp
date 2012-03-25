7df47b332ebcbbb82d810f04ad458096ff38c5d1
Ed Mackowiak <edmackowiak@gmail.com>
2012-03-24 21:40:07 -0700

    user adding is mostly working

--- app/controllers/meeting_controller.rb
+++ app/controllers/meeting_controller.rb
@@ -16,6 +16,14 @@ class MeetingController < ApplicationController
 
     @meeting.user = current_user
 
+    # deal with the selected checkboxes
+    if params[:attendees]
+      params[:attendees].each do |a|
+        @meeting.attendees.push Attendee.find(a)
+      end
+    end
+
+
     @meeting.save
     redirect_to dashboard_path, notice: "Successfully created meeting"
   end
@@ -32,6 +40,14 @@ class MeetingController < ApplicationController
     @meeting.end_time = params[:end_time]
     @meeting.location = params[:location]
     @meeting.agenda = params[:agenda]
+
+    @meeting.attendees.reset
+    # deal with the selected checkboxes
+    if params[:attendees]
+      params[:attendees].each do |a|
+        @meeting.attendees.push Attendee.find(a)
+      end
+    end
 
     @meeting.save
 
